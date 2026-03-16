760bb4aac3fbb8a47610c0fe1654c08d8ff9e756
nhwdev <nhw.dev@gmail.com>
2026-03-16 16:55:23 +0900

    MVC model2

--- model2study/src/main/java/model/DBConnection.java
+++ model2study/src/main/java/model/DBConnection.java
@@ -31,17 +31,4 @@ public class DBConnection {
 		session.commit();
 		session.close();
 	}
-
-	public static void close(Connection conn, Statement stmt, ResultSet rs) {
-		try {
-			if (rs != null)
-				rs.close();
-			if (stmt != null)
-				stmt.close();
-			if (conn != null)
-				conn.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 }
